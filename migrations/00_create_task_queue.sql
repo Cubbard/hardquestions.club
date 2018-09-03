4e80a9d6359999cfcba8463df01382f17b7276f6
gmanjames <yms1465@gmail.com>
2018-09-03 13:27:37 -0500

    Begin adding task edit form for King.

--- migrations/00_create_task_queue.sql
+++ migrations/00_create_task_queue.sql
@@ -11,7 +11,7 @@ create table task_queue (
     title       varchar(128) not null,
     descr       varchar(256) not null,
     is_head     int(1) not null default 0,
-    is_active   int(1) not null default 1,
+    is_active   int(1) not null default 0,
     queue_order bigint not null default 0,
     set_active  datetime null,
     expires     bigint not null
